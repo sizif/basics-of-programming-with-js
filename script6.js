2abc93b169799ec0468c008cc77604c341378ae3
ajdin <ajdika@live.com>
2016-09-26 13:54:44 +0000

    add String() Number() explicit, and + concat implicit coersion examples

---
+++ script6.js
@@ -0,0 +1,22 @@
+/*
+VALUE AND TYPE
+
+a = 42;
+a = String(a); // take the value of a, turn it into a string EXPLICITLY
+
+console.log(a); // returns black number (meaning its a string)
+console.log("hello world") // returns black text (cuz its also a string)
+
+b = Number(a); // returns blue string (meaning its a number)
+
+In JS, there's no such thing as a variable having a type.
+In JS, the value has the type, 
+
+IMPLICIT COERSIONS
+
+a = 42;
+a = a + "";
+console.log(a); // returns black
+
+
+*/
