a5cbb80893cd6f43b63516f18617e7ee9cea8566
ajdin <ajdika@live.com>
2016-09-27 08:28:31 +0000

    scope and es6

---
+++ script12.js
@@ -0,0 +1,15 @@
+// SCOPE
+/*
+You can always access variables out of your scope, but you can never
+reference variables that are further down, so that's a one-way street.
+*/
+
+// ECMASCRIPT6
+/*
+babeljs.io
+
+default parameters:
+function sayMsg(msg="This is a default message."){
+    console.log(msg);
+}
+*/
