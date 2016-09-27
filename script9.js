6208d4476bb1edaf107a2bc8b54f5dd2b68c8968
ajdin <ajdika@live.com>
2016-09-27 05:54:35 +0000

    falsy values in JS | void makes everything undefined

---
+++ script9.js
@@ -0,0 +1,20 @@
+/*
+"falsy" values in JS:
+
+0 -0 NaN
+""
+false
+null
+undefined
+
+
+The easiest ways to check if sth is going to become truthy or falsy:
+memorize this list and then consult it.
+
+Either it's on the above list, or the only other possible option is
+that it must be truthy.
+
+void is an operator that everything that you give it after it, becomes undefined.
+
+e.g. void 42, void 0, try it in the console
+*/
